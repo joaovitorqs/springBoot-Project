93caf373606e9a1d19892ae63bc3b05f198610da
João Vitor <joao_vqs@hotmail.com>
2026-04-13 14:14:50 -0300

    Update service UsuarioService

--- project/src/main/java/com/springProject/project/service/UsuarioService.java
+++ project/src/main/java/com/springProject/project/service/UsuarioService.java
@@ -1,9 +1,12 @@
 package com.springProject.project.service;
 
+import com.springProject.project.model.Usuario;
 import com.springProject.project.repository.UsuarioRepository;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UsuarioService {
@@ -14,5 +17,15 @@ public class UsuarioService {
     public UsuarioService (UsuarioRepository usuarioRepository){
         this.usuarioRepository = usuarioRepository;
         this.passwordEncoder = new BCryptPasswordEncoder();
+    }
+
+    public Usuario registarUsuario(String userName, String password){
+        String senhaCriptografada = passwordEncoder.encode(password);
+        Usuario usuario = new Usuario(userName, senhaCriptografada);
+        return usuarioRepository.save(usuario);
+    }
+
+    public Optional<Usuario> buscarPorUserName(String userName){
+        return usuarioRepository.findByUserName(userName);
     }
 }
