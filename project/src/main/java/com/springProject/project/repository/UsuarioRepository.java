7b272e3762962f50fbff190c9bb3b57032c6dd9e
João Vitor <joao_vqs@hotmail.com>
2026-04-13 13:22:41 -0300

    Add repository UsuarioRepository

---
+++ project/src/main/java/com/springProject/project/repository/UsuarioRepository.java
@@ -0,0 +1,14 @@
+package com.springProject.project.repository;
+
+import com.springProject.project.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.cdi.JpaRepositoryExtension;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByUsername(String userName);
+}
