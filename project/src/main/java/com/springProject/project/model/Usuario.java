df2ab589688616c507e9258b48fa46e19a29ff28
João Vitor <joao_vqs@hotmail.com>
2026-04-13 13:17:38 -0300

    Add model Usuario

---
+++ project/src/main/java/com/springProject/project/model/Usuario.java
@@ -0,0 +1,46 @@
+package com.springProject.project.model;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.AnyDiscriminatorImplicitValues;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String userName;
+
+    @Column(nullable = false)
+    private String password;
+
+    public Usuario (){}
+
+    public Usuario(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
